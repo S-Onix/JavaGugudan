8ed249f0614c5431f580359a5f82edd515250840
yms <yms@DESKTOP-7T9O25Q>
2017-11-10 12:37:12 +0900

    variable 사용, Scanner를 통한 입력

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
 
 public class Gugudan {
 	public static void main(String [] args) {
@@ -22,5 +25,46 @@ public class Gugudan {
 		System.out.println(3*7);
 		System.out.println(3*8);
 		System.out.println(3*9);
+		
+		int number3 = 3 *1;
+		System.out.println(number3);
+		
+		int number3 = 3 *2;
+		System.out.println(number3);
+		
+		int number3 = 3 *3;
+		System.out.println(number3);
+		
+		int number3 = 3 *4;
+		System.out.println(number3);
+		
+		int number3 = 3 *5;
+		System.out.println(number3);
+		
+		int number3 = 3 *6;
+		System.out.println(number3);
+		
+		int number3 = 3 *7;
+		System.out.println(number3);
+		
+		int number3 = 3 *8;
+		System.out.println(number3);
+		
+		int number3 = 3 *9;
+		System.out.println(number3);
+		
+		
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
+	
 	}
 }
