58a9932ecd9d9ea39ad4d640c42c34957a3e7c8d
yms <yms@DESKTOP-7T9O25Q>
2017-11-13 12:08:23 +0900

    if문, 입력구구단

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,80 +1,22 @@
 import java.util.Scanner;
 
-import javax.swing.plaf.synth.SynthSpinnerUI;
-
 public class Gugudan {
 	public static void main(String [] args) {
-		//2단
-		System.out.println(2*1);
-		System.out.println(2*2);
-		System.out.println(2*3);
-		System.out.println(2*4);
-		System.out.println(2*5);
-		System.out.println(2*6);
-		System.out.println(2*7);
-		System.out.println(2*8);
-		System.out.println(2*9);
-
-
-		System.out.println(3*1);
-		System.out.println(3*2);
-		System.out.println(3*3);
-		System.out.println(3*4);
-		System.out.println(3*5);
-		System.out.println(3*6);
-		System.out.println(3*7);
-		System.out.println(3*8);
-		System.out.println(3*9);
-		
-		int number3 = 4 *1;
-		System.out.println(number3);
-		
-		number3 = 4 *2;
-		System.out.println(number3);
-		
-		number3 = 4 *3;
-		System.out.println(number3);
-		
-		number3 = 4 *4;
-		System.out.println(number3);
-		
-		number3 = 4 *5;
-		System.out.println(number3);
-		
-		number3 = 4 *6;
-		System.out.println(number3);
-		
-		number3 = 4 *7;
-		System.out.println(number3);
-		
-		number3 = 4 *8;
-		System.out.println(number3);
-		
-		number3 = 4 *9;
-		System.out.println(number3);
-		
-		
-		Scanner scan = new Scanner(System.in);
-		int number = scan.nextInt();
-		System.out.println(number * 1);
-		System.out.println(number * 2);
-		System.out.println(number * 3);
-		System.out.println(number * 4);
-		System.out.println(number * 5);
-		System.out.println(number * 6);
-		System.out.println(number * 7);
-		System.out.println(number * 8);
-		System.out.println(number * 9);
-		
-		int i = 1;
-		while(i<10) {
-			System.out.println(6*i);
-			i++;
-		}
-		
-		for(int j = 1 ; j < 10 ; j++) {
-			System.out.println(j*7);
+	
+		boolean flag = true;
+		while(flag) {
+			System.out.println("구구단 출력할 단은?(0 입력시 종료) ");
+			Scanner scan = new Scanner(System.in);
+			int input = scan.nextInt();
+			if(input >0 && input <10) {
+				for(int i = 1; i < 10; i++) {
+					System.out.println(input * i);
+				}
+			}else if(input == 0){
+				flag = false;
+			}
+			else System.out.println("2이상 9 이하를 입력해주세요");
+			}
 		}
 	
-	}
 }
