493983d437801d1a2afb49f814634263297d7a72
yms <yms@DESKTOP-7T9O25Q>
2017-11-13 22:59:53 +0900

    method 사용

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
 public class Gugudan {
-	public static void main(String [] args) {
 	
-//		Scanner scan = new Scanner(System.in);
-//		// int 타입을 가진 result라는 이름을 가진 배열 , 크기가 9인 배열을 생성
+	public static int[] gugudan(int input) {
 		int [] result = new int[9];
-//		int input = scan.nextInt();
-//		
-		for(int j = 2; j < 10; j++) {
-			for(int i = 0 ; i < result.length; i++) {
-				result[i] = j *(i+1);
-				System.out.println(result[i]);
-			}
+		for(int i = 0; i < result.length ; i++) {
+			result[i] = input * (i+1); 
 		}
+		return result;
+	}
+	public static void print(int [] result) {
+		for(int i = 0; i< result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String [] args) {		
 		
-		
+		for(int i = 2 ; i < 10 ; i++) {
+			int [] result = gugudan(i);
+			print(result);
+		}
 		
 	}
 	
