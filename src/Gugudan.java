5d1c77215ab7ac9a9047d6819781033855203c53
yms <yms@DESKTOP-7T9O25Q>
2017-11-13 13:06:06 +0900

    2중 for문

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Gugudan {
 	public static void main(String [] args) {
 	
-		boolean flag = true;
-		while(flag) {
-			System.out.println("구구단 출력할 단은?(0 입력시 종료) ");
-			Scanner scan = new Scanner(System.in);
-			int input = scan.nextInt();
-			if(input >0 && input <10) {
-				for(int i = 1; i < 10; i++) {
-					System.out.println(input * i);
-				}
-			}else if(input == 0){
-				flag = false;
-			}
-			else System.out.println("2이상 9 이하를 입력해주세요");
+//		Scanner scan = new Scanner(System.in);
+//		// int 타입을 가진 result라는 이름을 가진 배열 , 크기가 9인 배열을 생성
+		int [] result = new int[9];
+//		int input = scan.nextInt();
+//		
+		for(int j = 2; j < 10; j++) {
+			for(int i = 0 ; i < result.length; i++) {
+				result[i] = j *(i+1);
+				System.out.println(result[i]);
 			}
 		}
+		
+		
+		
+	}
 	
 }
