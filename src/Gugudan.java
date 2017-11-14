30f5c243672d82e8cb6bfda4cfe2a645abf08d02
yms <yms@DESKTOP-7T9O25Q>
2017-11-14 23:41:22 +0900

    AxA 단 완성, AxB단 완성

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,15 +1,29 @@
 public class Gugudan {
 	
-	public static int[] gugudan(int input) {
-		int [] result = new int[9];
-		for(int i = 0; i < result.length ; i++) {
-			result[i] = input * (i+1); 
+	public static int[][] gugudan(int input) {
+		int [][] result = new int[input-1][input];
+		for(int i = 2; i < input+1 ; i++) {
+			for(int j = 0 ; j < input; j++) {
+			result[i-2][j] = i* (j+1); 
+			}
 		}
 		return result;
 	}
-	public static void print(int [] result) {
+	
+	public static int[][] gugudan(int number1, int number2) {
+		int [][] result = new int[number1-1][number2];
+		for(int i = 2; i < number1 +1 ; i++) {
+			for(int j = 0 ; j < number2; j++) {
+			result[i-2][j] = i* (j+1); 
+			}
+		}
+		return result;
+	}
+	
+	public static void print(int [][] result) {
 		for(int i = 0; i< result.length; i++) {
-			System.out.println(result[i]);
+			for(int j = 0; j < result[i].length; j++)
+			System.out.println(result[i][j]);
 		}
 	}
 }
